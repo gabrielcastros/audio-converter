44465b4d460cf769e05378cf48a888951a3d736c
gabrielsoaresdecastro <gabrielsoaresdecastro@gmail.com>
2025-08-26 23:42:12 -0300

    initial commit: only mp3 format available

---
+++ src/main/java/com/gabrielcastro/audioconverter/AudioConverter.java
@@ -0,0 +1,21 @@
+package com.gabrielcastro.audioconverter;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+
+public class AudioConverter {
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
+
+        AudioLoader loader = new AudioLoader();
+
+        File dir = new File("/home/gabriel/Documentos/cd/audio-converter/files");
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            String path = file.getAbsolutePath();
+            loader.convertToMp3(path);
+        }
+    }
+}
---
+++ src/main/java/com/gabrielcastro/audioconverter/AudioLoader.java
@@ -0,0 +1,37 @@
+package com.gabrielcastro.audioconverter;
+
+import it.sauronsoftware.jave.AudioAttributes;
+import it.sauronsoftware.jave.Encoder;
+import it.sauronsoftware.jave.EncodingAttributes;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+public class AudioLoader {
+
+    Encoder encoder = new Encoder();
+
+    public void convertToMp3(String path) throws UnsupportedAudioFileException, IOException {
+        File sourceFile = new File(path);
+        String newPath = path.replace(sourceFile.getName(), sourceFile.getName().replace(".wav", ".mp3"));
+        File outputFile = new File(newPath);
+        try {
+            AudioAttributes audio = new AudioAttributes();
+            audio.setCodec("libmp3lame");
+            audio.setBitRate(64000);
+            audio.setChannels(2);
+            audio.setSamplingRate(44100);
+
+            EncodingAttributes attrs = new EncodingAttributes();
+            attrs.setFormat("mp3");
+            attrs.setAudioAttributes(audio);
+
+            Encoder encoder = new Encoder();
+            encoder.encode(sourceFile, outputFile, attrs);
+            System.out.printf("Sucess! Filename: %s%n", outputFile.getName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
